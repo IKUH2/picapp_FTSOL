25d1a397a3fbd548d614b6cddf3219a4bae87c28
IKUH2 <yikuh2@gmail.com>
2020-09-24 20:58:27 +0900

    README追加

--- app/javascript/home/home.js
+++
@@ -1,5 +0,0 @@
-const imageArea = document.getElementById('imageArea');
-const images = ['ani10.JPG', 'ani11.JPG', 'ani12.JPG'];
-
-const imageNo = Math.floor( Math.random() * images.length)
-imageArea.src = images[imageNo];
